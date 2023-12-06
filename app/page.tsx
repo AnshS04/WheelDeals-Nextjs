b11b5a95a7ca70c4397d1479fae07987549f2891
AnshSachdeva23 <sachdevaansh2002@gmail.com>
2023-12-06 19:34:53 +0530

    added CarCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 
-import { CustomFilter, Footer, Hero, Navbar, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Footer, Hero, Navbar, SearchBar } from '@/components'
+import { fetchCars } from '@/utils'
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Navbar />
@@ -20,6 +23,19 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (<CarCard car={car} />))}
+            </div>
+          </section>
+        ) : (
+          <div className='home__error-container'>
+            <h2 className='text-blac text-xl font-bold'>No cars found</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
       <Footer />
     </main>
